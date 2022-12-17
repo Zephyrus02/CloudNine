670177623e593bf9e0ebb1f9f486fc6d8e7b4f7d
Zephyrus02 <aneeshraskar@gmail.com>
2022-12-18 03:09:27 +0530

    Removed SQL Linking, Improved CSS

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import mysql.connector
 app = Flask(__name__)
 
 # home function to call homepage for user
@@ -22,30 +21,12 @@ def guide():
     guide.MED = med
     guide.CLOTH = cloth
     guide.NOTE = note
-    return render_template('guide.html')
+    return render_template('guide.html', Name=name, Calamity=calamity, FW=fw, Med=med, Cloth=cloth, Note=note)
 
 # display function to call the display page for the responsible authorities
 @app.route("/display", methods=['GET', 'POST'])
 def display():
-
-    mydb = mysql.connector.connect(host="loaclhost", user="root", passwd="1234", database='provider1')
-    fws = 0
-    ms = 0
-    cls = 0
-    mycursor = mydb.cursor()
-    mycursor.execute(" select quantity from inventory where category='Food' ")
-    for i in mycursor:
-        fws += i
-
-    mycursor.execute(" select quantity from inventory where category='Med' ")
-    for i in mycursor:
-        ms += i
-
-    mycursor.execute(" select quantity from inventory where category='Cloth' ")
-    for i in mycursor:
-        cls += i
-
-    return render_template('display.html', Name=guide.NAME, Calamity=guide.CALAMITY, FW=guide.FW, FWS=fws, MS=ms, CLS=cls, Med=guide.MED,
+    return render_template('display.html', Name=guide.NAME, Calamity=guide.CALAMITY, FW=guide.FW, Med=guide.MED,
                            Cloth=guide.CLOTH, Note=guide.NOTE)
 
 app.run(debug=True)
