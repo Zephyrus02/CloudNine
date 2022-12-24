163e2b62fa75eec5c73a335d4b179495c407cec8
Zephyrus02 <aneeshraskar@gmail.com>
2022-12-24 19:31:50 +0530

    Added Geolocation to get location of device ip

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import requests
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -24,17 +25,33 @@ def guide():
     guide.MED = med
     guide.CLOTH = cloth
     guide.NOTE = note
+
+    r = requests.get('https://www.geojs.io')
+    print(r)
+
+    ipreq = requests.get('https://get.geojs.io/v1/ip.json')
+    ipadd = ipreq.json()['ip']
+    print(ipadd)
+
+    url = 'https://get.geojs.io/v1/ip/geo/' + ipadd + '.json'
+    geo_req = requests.get(url)
+    geodata = geo_req.json()
+    print(geodata)
+    print(geodata['latitude'])
+    print(geodata['longitude'])
+    print(geodata['timezone'])
+    print(geodata['country'])
+
     return render_template('guide.html', Name=name, Calamity=calamity, FW=fw, Med=med, Cloth=cloth, Note=note)
 
 
 # display function to call the display page for the responsible authorities
 @app.route("/display", methods=['GET', 'POST'])
 def display():
-    db = mysql.connector.connect(host="localhost", user="root", password="root", database="provider1")
-    cursor = db.cursor()
-    cursor.execute("SELECT quantity FROM INVENTORY where item='food'")
-    i = 0
-    for i in cursor:
+    db = mysql.connector.connect(host="localhost", user="root", password="root", database="provider")
+    mycursor = db.cursor()
+    mycursor.execute("SELECT quantity FROM INVENTORY where item='food'")
+    for i in mycursor:
         print(i[0])
     return render_template('display.html', Name=guide.NAME, Calamity=guide.CALAMITY, FW=guide.FW, Med=guide.MED,
                            Cloth=guide.CLOTH, Note=guide.NOTE, IO=i[0])
